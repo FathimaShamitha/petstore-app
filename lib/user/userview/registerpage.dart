80064d904589ba0f83a7894a20f8961f6d893413
FathimaShamitha <fathimashamitha7@gmail.com>
2023-11-06 13:25:52 +0530

    initialsetup

---
+++ lib/user/userview/registerpage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RegisterClass extends StatefulWidget{
+  State<RegisterClass> createState()=> RegisterClassState();
+}
+
+class RegisterClassState extends State<RegisterClass>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightGreen,
+      ),
+    );
+  }
+}
