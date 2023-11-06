00722b36278900758028c6d248d13fba910eaea9
FathimaShamitha <fathimashamitha7@gmail.com>
2023-11-06 14:02:20 +0530

    registerview

--- lib/user/userview/registerpage.dart
+++ lib/user/userview/registerpage.dart
@@ -1,15 +1,116 @@
 import 'package:flutter/material.dart';
 
-class RegisterClass extends StatefulWidget{
-  State<RegisterClass> createState()=> RegisterClassState();
+class RegisterClass extends StatefulWidget {
+  State<RegisterClass> createState() => RegisterClassState();
 }
 
-class RegisterClassState extends State<RegisterClass>{
+class RegisterClassState extends State<RegisterClass> {
+  GlobalKey<FormState> formKey = GlobalKey();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.lightGreen,
+        backgroundColor: Colors.orange,
+        title: Text(
+          "PetStore",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 50, bottom: 20),
+                child: Text(
+                  "Register",
+                  style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.orange),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30, bottom: 20),
+                child: TextFormField(
+                  controller: nameController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Field cannot be empty!!!";
+                    }
+                  },
+                  decoration: InputDecoration(
+                      hintText: "Name",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30, bottom: 20),
+                child: TextFormField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Field cannot be empty!!!";
+                    }
+                  },
+                  decoration: InputDecoration(
+                      hintText: "Email",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30, bottom: 20),
+                child: TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  obscuringCharacter: "*",
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Field cannot be empty!!!";
+                    }
+                  },
+                  decoration: InputDecoration(
+                      hintText: "Password",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30, bottom: 40),
+                child: TextFormField(
+                  controller: phoneController,
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Field cannot be empty!!!";
+                    }
+                  },
+                  decoration: InputDecoration(
+                      hintText: "PhoneNo",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5))),
+                ),
+              ),
+              ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.orange),
+                  onPressed: () {},
+                  child: Text(
+                    "Register",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ))
+            ],
+          ),
+        ),
       ),
     );
   }
